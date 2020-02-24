b9ccd152ebd0e7c9f353b266e013d69e6ee073d7
Pratik <pr.tikgohil@gmail.com>
2020-02-24 17:41:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(array, callback) {
+  array.forEach(elem => {
+    callback(elem)
+  })
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(array) {
+  array.forEach(elem => alert(elem))
+}
